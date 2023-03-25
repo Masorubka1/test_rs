786679cc18d7e9b5f75d940521017664181a5973
Мальцев Андрей <masorubka@Masorubka.local>
2023-03-25 17:37:51 +0300

    left static

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,63 @@
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread;
 use std::time::Instant;
 
-fn split_computation<T: Send + Sync + Clone + 'static, R: Send + 'static>(
+fn split_computation<T: Send + 'static, R: Send + 'static>(
     input: Vec<T>,
     f: fn(T) -> R,
-    threshold: usize,
+    _threshold: usize,
 ) -> Vec<R> {
-    if input.len() <= threshold {
-        // If input is short enough, compute f sequentially
-        input.into_iter().map(f).collect()
-    } else {
-        // Otherwise, split input and compute f in parallel
-        let num_threads = 10; // num_cpus::get(); // Number of CPU cores
-        let chunk_size = (input.len() + num_threads - 1) / num_threads;
-        let chunks = input.chunks(chunk_size);
-
-        let mut vec_threads = Vec::with_capacity(num_threads);
-        for chunk in chunks {
-            let inp = chunk.to_vec();
+    let input_len = input.len();
+    let num_threads = 10; // num_cpus::get(); // Number of CPU cores
+    let chunk_size = (input.len() + num_threads - 1) / num_threads;
+    
+    let (tx, rx) = channel();
+    let mut vec_threads = Vec::with_capacity(num_threads);
+    
+    let mut tmp = Vec::new();
+    let mut cnt = 0;
+    for i in input {
+        if input_len > _threshold && cnt == chunk_size {
+            cnt = 0;
             let local_f = f;
-            let th = thread::spawn(move || {
-                inp.into_iter().map(local_f).collect::<Vec<_>>()
+            let local_tx = tx.clone();
+            let th = thread::spawn( move || {
+                for i in tmp {
+                    let res = local_f(i);
+                    local_tx.send(res).unwrap();
+                }
             });
             vec_threads.push(th);
+            tmp = Vec::new();
         }
-
-        let mut results = Vec::with_capacity(input.len());
-        for th in vec_threads {
-            let chunk_result = th.join().unwrap().into_iter();
-            results.extend(chunk_result);
-        }
-        results
+        tmp.push(i);
+        cnt += 1;
     }
+    if tmp.len() > 0 {
+        let local_f = f;
+        let local_tx = tx.clone();
+        let th = thread::spawn( move || {
+            for i in tmp {
+                let res = local_f(i);
+                local_tx.send(res).unwrap();
+            }
+        });
+        vec_threads.push(th);
+    }
+
+    drop(tx);
+
+    let mut results = Vec::with_capacity(input_len);
+    for th in vec_threads {
+        th.join().unwrap();
+        for val in rx.iter() {
+            let tmp = val;
+            results.push(tmp);
+        }
+    }
+    results
 }
-
-
 
 mod test {
     use super::*;
@@ -45,25 +68,28 @@ mod test {
     }
 
     const INP: [isize; 6] = [1, 2, 3, 4, 5, 6];
+    const OUT: [isize; 6] = [1, 4, 9, 16, 25, 36];
 
     #[test]
     fn test_2() {
         let start = Instant::now();
-        let out = split_computation(INP.to_vec(),  sum,  2);
+        let mut out = split_computation(INP.to_vec(),  sum,  2);
+        out.sort();
         let duration = start.elapsed();
         println!("time: {:?}", duration);
-        
-        assert_eq!(out, [1, 4, 9, 16, 25, 36]);
+
+        assert_eq!(out, OUT);
     }
 
     #[test]
     fn test_5() {
         let start = Instant::now();
-        let out = split_computation(INP.to_vec(),  sum,  6 - 1);
+        let mut out = split_computation(INP.to_vec(),  sum,  6 - 1);
+        out.sort();
         let duration = start.elapsed();
         println!("time: {:?}", duration);
 
-        assert_eq!(out, [1, 4, 9, 16, 25, 36]);
+        assert_eq!(out, OUT);
     }
 
     #[test]
@@ -73,6 +99,21 @@ mod test {
         let duration = start.elapsed();
         println!("time: {:?}", duration);
 
-        assert_eq!(out, [1, 4, 9, 16, 25, 36]);
+        assert_eq!(out, OUT);
     }
 }
+
+/*fn main() {
+    fn sum(a: isize) -> isize {
+        thread::sleep(std::time::Duration::from_millis(400));
+        return a * a
+    }
+
+    const INP: [isize; 6] = [1, 2, 3, 4, 5, 6];
+    const OUT: [isize; 6] = [1, 4, 9, 16, 25, 36];
+
+    let start = Instant::now();
+    let out = split_computation(INP.to_vec(),  sum,  2);
+    let duration = start.elapsed();
+    println!("time: {:?}", duration);
+}*/
